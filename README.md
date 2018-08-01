b520b8ece82747ce022b19189b9f5a1337e823b2
Daniel Hines <dhines@regent.edu>
2018-08-01 09:28:31 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # dynamicscrm-proxy-server
+
 A simple proxy server that allows a client to bypass NTLM authentication with an On-Premise instance of Dynamics CRM
+
+## Quick Start
+
+``` bash
+dynamics-crm-proxy -u yourusername -p yourpassword -a https://your.domain.com/YOURORG/api/data/v8.0/
+```
+
+Run `dynamics-crm-proxy --help` for more info/examples, or keep reading below.
+
+## Configuration
+
+Before using the proxy, you must configure it with the CRM's api url and the credentials you want it to use. You may also optionally configure a port the proxy should listen on (defaults to 3000).
+
+There are two ways you can configure the proxy server. You may:
+
+- Set environmental variables `CRMPROXY_USERNAME`, `CRMPROXY_PASSWORD`, `CRMPROXY_API_URL`, and, optionally, `CRMPROXY_PORT`
+- Pass in command-line arguments. Here's the Yargs output:
+
+  ``` bash
+  Usage: index.js [options]
+
+  Options:
+  --help          Show help                                            [boolean]
+  --version       Show version number                                  [boolean]
+  --username, -u  CRM Username                                        [required]
+  --password, -p  CRM Password                                        [required]
+  --apiUrl, -a    CRM API URL                                         [required]
+  --verbose, -v   Verbose Log Level                                    [boolean]
+  --port          Port the proxy server should liston on
+
+  Examples:
+    index.js -u user1 -p mypass1337 -o
+    https://internalcrm.regent.edu/CRMRECRUIT/api/data/v8.0 -v
+    index.js --username user1 -password mypass1337 -apiurl
+    https://advisedevcrm.regent.edu/CRMADVISETEST/api/data/v8.1/ --port 1337
+
+  ```
+
---
+++ index.js
@@ -0,0 +1,123 @@
+#! /usr/bin/env node
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const chalk = require('chalk');
+const ntlm = require('request-ntlm-continued');
+
+const argv = require('yargs')
+  .usage('Usage: $0 [options]')
+  .env('CRMPROXY')
+  .describe({
+    username: 'CRM Username',
+    password: 'CRM Password',
+    apiUrl: 'CRM API URL',
+    verbose: 'Verbose Log Level',
+    port: 'Port the proxy server should liston on'
+  })
+  .example('$0 -u user1 -p mypass1337 -o https://internalcrm.mydomain.com/CRMRECRUIT/api/data/v8.0 -v')
+  .example('$0 --username user1 -password mypass1337 -apiurl https://salescrm.mydomain.com/SALESCRM/api/data/v8.1/ --port 1337')
+  .boolean('v')
+  .alias({
+    username: 'u',
+    password: 'p',
+    apiUrl: 'a',
+    verbose: 'v'
+  })
+  .demandOption([
+    'username', 'password',
+    'apiUrl'
+  ]).argv;
+
+const VERBOSE_FLAG = argv.verbose;
+const APIURL = argv.apiUrl;
+const USERNAME = argv.username
+const PASSWORD = argv.password;
+const PORT = argv.port || 3000
+
+/**
+ * @param {string} message - the message to be logged.
+ * @param {boolean} verbose - boolean that sets the verbose level of the log item.
+ */
+function log(message, verbose) {
+  if (!verbose || VERBOSE_FLAG) {
+    console.log(message);
+  }
+}
+
+const HEADERS = {
+  'Accept': 'application/json',
+  'OData-MaxVersion': '4.0',
+  'OData-Version': '4.0',
+  'Prefer': 'odata.include-annotations="*"',
+  'Content-Type': 'application/json; charset=utf-8'
+}
+
+/** Performs an NTLM enabled request against the CRM, then passes the response to the callback */
+function CRMRequest({
+  method,
+  url,
+  headers,
+  body
+}, cb) {
+  delete headers.host;
+  var opts = {
+    username: USERNAME,
+    password: PASSWORD,
+    headers: HEADERS,
+    url: APIURL + url,
+    ntlm_domain: 'REGENTNT',
+    workstation: process.env.COMPUTERNAME,
+  }
+  log(chalk.yellow(`Forwarding ${method} to ${url}`), true);
+  ntlm[method.toLowerCase()](opts, body || null, (err, res, resBody) => {
+    if (err) {
+      throw err;
+    }
+    cb(res);
+  });
+}
+
+function startServer() {
+  // This line tells express to parse the body of the request as JSON, giving us access to it later
+  app.use(express.json());
+  // This middleware enables CORS
+  app.use(cors({
+    exposedHeaders: 'odata-entityid'
+  }));
+
+  app.all('/*', (origReq, origRes) => {
+    CRMRequst(origReq, ({
+      headers,
+      body,
+      statusCode,
+      statusMessage
+    }) => {
+      const id = headers['odata-entityid'] || headers['OData-EntityId']
+      if (headers && id) {
+        origRes.setHeader('odata-entityid', id);
+      }
+      origRes.status(statusCode);
+      origRes.statusMessage = statusMessage;
+      origRes.send(body);
+      log(chalk.green('Response sent!'), true);
+    });
+  });
+  app.listen(PORT, () => log('CRM Web API Proxy listening on port 3000!'));
+}
+
+// Perform a test request to validate the configuration is correct, 
+// then start the server
+CRMRequest({
+  method: 'GET',
+  url: 'WhoAmI()',
+  headers: HEADERS
+}, ({
+  status,
+  statusCode,
+  body
+}) => {
+  log(chalk.green('Successfully connected to ' + APIURL));
+  startServer();
+});
